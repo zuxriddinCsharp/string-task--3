1216e1112dc84e4b450b1e2cae83c13daf738084
zuxriddinCsharp <137428899+zuxriddinCsharp@users.noreply.github.com>
2023-07-19 23:41:26 +0500

    Добавьте файлы проекта.

---
+++ string task -3/Program.cs
@@ -0,0 +1,28 @@
+﻿
+namespace string_task__3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter string: ");
+            string words = Console.ReadLine();
+
+            int maxWords = 0;
+
+            string[] arrayWord = words.Split(' ');
+
+            string longestWord = "";
+
+            foreach (string word in arrayWord)
+            {
+                if (word.Length > maxWords)
+                {
+                    maxWords = word.Length;
+                    longestWord = word;
+                }
+            }
+            Console.WriteLine($"Result: Longest word {longestWord} out of {maxWords} letters ");
+        }
+    }
+}
